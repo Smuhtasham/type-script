fd29ad4dcb9c189c148092cfeeb4a351ac620df3
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-10-26 14:33:10 +0500

    fix

--- components/typescript.tsx
+++ components/typescript.tsx
@@ -23,20 +23,42 @@
 // ------------------------------------------
 
 // function add(a: number, b: number): undefined | number | void {
-function add(a: number, b: number) {
-  const result = a + b;
-  // console.log(result);
-  return result;
-}
+// function add(a: number, b: number) {
+//   const result = a + b;
+//   // console.log(result);
+//   return result;
+// }
 
-type CalFn = (a: number, b: number) => number;
+// type CalFn = (a: number, b: number) => number;
 
-function calculate(
-  a: number,
-  b: number,
-  cal: CalFn
-) {
-  cal(a, b);
-}
+// function calculate(
+//   a: number,
+//   b: number,
+//   cal: CalFn 
+// ) {
+//   cal(a, b);
+// }
 
-calculate(2, 3, add);
+// calculate(2, 3, add);
+
+// let role: 'admin' | 'user' | 'editor';
+// role='hello'
+
+// type DataStorage<T> = {
+//   storage:T[],
+//   add : (data: T) => T[];
+// }
+
+// const textStorage:DataStorage<string> ={
+//   storage:[],
+//   add(data) {
+//     this.storage.push(data);
+//   }
+// }
+
+// const UserStorage: DataStorage<User> ={
+//   storage:[],
+//   add(user){
+
+//   }
+// }
