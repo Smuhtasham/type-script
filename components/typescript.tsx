6c3d7ecfcc7392becca54b7075a1a80e6643efc4
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-11-15 13:00:54 +0500

    fixed

--- components/typescript.tsx
+++ components/typescript.tsx
@@ -71,7 +71,7 @@
 // type Container<T extends ElementType> = {
 //   as: T;
 //   children:React.ReactNode;
-// } & ComponentPropsWithoutRef<T>  ;
+// } & ComponentPropsWithoutRef<T>
 // export default function Container<C extends ElementType>({ as,children,...props }: Container<C>) {
 //   const Component = as || 'div';
 //   return <Component {...props}>{children}</Component>;
